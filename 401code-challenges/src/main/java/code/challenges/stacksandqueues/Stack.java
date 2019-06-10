701b0cc0abf7c3a2454a00df0cc20cc6a195ccd8
Robert Bronson <robert.j.bronson+cf@gmail.com>
2019-06-10 13:30:01 -0700

    stack class written, still writing tests, and need to do readme

--- 401code-challenges/src/main/java/code/challenges/stacksandqueues/Stack.java
+++ 401code-challenges/src/main/java/code/challenges/stacksandqueues/Stack.java
@@ -1,4 +1,35 @@
 package code.challenges.stacksandqueues;
 
+import code.challenges.linkedlist.Node;
+
 public class Stack {
+    Node top;
+
+    //Constructor for stack
+    public Stack(){
+        this.top = null;
+    }
+
+    //Add a node to the top of the stack with input value.
+    public void push(int val){
+
+    }
+
+    //Pop off the top node and return its value.
+    public int pop() throws NullPointerException {
+        if (this.top == null){
+            throw new NullPointerException("The stack is empty.");
+        }
+        int temp = this.peek();
+        this.top = this.top.getNext();
+        return temp ;
+    }
+
+    //Return the value of the top node.
+    public int peek() throws NullPointerException {
+        if (this.top == null){
+            throw new NullPointerException("The stack is empty.");
+        }
+        return this.top.getValue();
+    }
 }
---
+++ 401code-challenges/src/test/java/code/challenges/stacksandqueues/StackTest.java
@@ -0,0 +1,38 @@
+package code.challenges.stacksandqueues;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StackTest {
+
+    @Test
+    public void test_push() {
+        Stack testStack = new Stack();
+        
+    }
+
+    @Test
+    public void test_multiple_push() {
+    }
+
+    @Test
+    public void test_pop() {
+    }
+
+    @Test
+    public void test_multiple_pop() {
+    }
+
+    @Test
+    public void test_pop_empty() {
+    }
+
+    @Test
+    public void test_peek() {
+    }
+
+    @Test
+    public void test_peek_empty() {
+    }
+}
