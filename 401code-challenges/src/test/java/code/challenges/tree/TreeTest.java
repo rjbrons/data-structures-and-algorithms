ccb5c8063aeae5e3fc52bf316ebd81cef69c846c
Robert Bronson <robert.j.bronson+cf@gmail.com>
2019-06-19 21:12:50 -0700

    finished challenge.

--- 401code-challenges/src/test/java/code/challenges/tree/TreeTest.java
+++ 401code-challenges/src/test/java/code/challenges/tree/TreeTest.java
@@ -102,4 +102,31 @@ public class TreeTest {
         testTree.breadthFirst(testTree);
         assertEquals("1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n",outContent.toString() );
     }
+
+    @Test
+    public void testMaxValue(){
+        BinaryTree<Integer> testTree = new BinaryTree<>();
+        testTree.setRoot(new TreeNode<Integer>(1));
+        testTree.getRoot().setLeft(new TreeNode<Integer>(2));
+        testTree.getRoot().getLeft().setLeft(new TreeNode<Integer>(4));
+        testTree.getRoot().getLeft().getLeft().setLeft(new TreeNode<Integer>(8));
+        testTree.getRoot().getLeft().getLeft().setRight(new TreeNode<Integer>(9));
+        testTree.getRoot().getLeft().setRight(new TreeNode(5));
+        testTree.getRoot().getLeft().getRight().setLeft(new TreeNode<Integer>(40));
+        testTree.getRoot().getLeft().getRight().setRight(new TreeNode<Integer>(11));
+        testTree.getRoot().setRight(new TreeNode<Integer>(3));
+        testTree.getRoot().getRight().setLeft(new TreeNode(6));
+        testTree.getRoot().getRight().getLeft().setRight(new TreeNode<Integer>(12));
+        testTree.getRoot().getRight().setRight(new TreeNode<Integer>(7));
+        testTree.getRoot().getRight().getRight().setLeft(new TreeNode<Integer>(13));
+        testTree.getRoot().getRight().getRight().setRight(new TreeNode<Integer>(14));
+
+        assertEquals("Should return the value 40", (long)40, (long)testTree.maxValue(testTree));
+    }
+
+    @Test
+    public void testMaxOnEmpty(){
+        BinaryTree<Integer> testTree = new BinaryTree<>();
+        assertEquals((long)Integer.MIN_VALUE,(long)testTree.maxValue(testTree));
+    }
 }
