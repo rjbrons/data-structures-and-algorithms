72cfb9e46064bcd41a681089d50bacd8ea42824a
Robert Bronson <robert.j.bronson+cf@gmail.com>
2019-07-17 13:09:14 -0700

    refactoring for using a string

---
+++ 401code-challenges/src/main/java/code/challenges/Graph/GetEdge/Edge.java
@@ -0,0 +1,37 @@
+package code.challenges.Graph.GetEdge;
+
+
+public class Edge {
+    int weight;
+    Vertex destination;
+
+    public Edge(){};
+
+    protected Edge(Vertex dest){
+        this.destination = dest;
+        this.weight = 1;
+    }
+
+    protected Edge(int wt, Vertex dest){
+        this.weight = wt;
+        this.destination = dest;
+    }
+
+    // Getters and Setters
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public Vertex getDestination() {
+        return destination;
+    }
+
+    public void setDestination(Vertex destination) {
+        this.destination = destination;
+    }
+
+}
